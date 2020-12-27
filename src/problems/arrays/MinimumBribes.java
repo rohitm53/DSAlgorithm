ab2c5fb69d37a61da712284e7f78df37a41ca16a
rohitm <rohit.manohar@tomra.com>
2020-12-27 18:46:32 +0530

    f

--- src/problems/arrays/MinimumBribes.java
+++ src/problems/arrays/MinimumBribes.java
@@ -1,57 +1,44 @@
 package problems.arrays;
 
+import java.util.Arrays;
+
 public class MinimumBribes {
 
-//    static void minimumBribes(int[] q) {
-//        int bribes=0;
-//        boolean isChaotic=false;
-//
-//        for(int i=0;i<q.length;i++){
-//            int bribe = q[i]-(i+1);
-//            if(bribe>2){
-//                isChaotic=true;
-//                 break;
-//            }else{
-//                if(bribe>0){
-//                    bribes+= bribe;
-//                }else{
-//                  int expectedValue = i+1;
-//                  int currentValue = q[i];
-//
-//                    System.out.println("expectedValue = "+expectedValue+"\n" +
-//                            "currentValue = "+currentValue);
-//                }
-//            }
-//        }
-//
-//        if(isChaotic){
-//            System.out.println("Too chaotic");
-//        }else{
-//            System.out.println(bribes);
-//        }
-//    }
-
     static void minimumBribes(int[] q) {
-        int bribes=0;
-        boolean isChaotic=false;
 
-        for(int i=0;i<q.length;i++){
-            int bribe = q[i]-(i+1);
-            if(bribe>2){
-                isChaotic=true;
-                 break;
+        int len = q.length;
+        int bribes=0;
+        boolean isChaotic = false;
+
+        for(int i=(len-1);i>0;i--){
+
+            int expectedValue=(i+1);
+            int currentValue=q[i];
+
+            if(currentValue==expectedValue){
+            }else if(q[i-1]==expectedValue){
+                swap(q,(i-1),i);
+                bribes++;
+            }else if(q[i-2]==expectedValue){
+                swap(q,(i-1),(i-2));
+                swap(q,(i-1),i);
+                bribes=bribes+2;
+
             }else{
-                if(bribe>0){
-                    bribes+=bribe;
-                }
+                isChaotic=true;
+                System.out.println("Too chaotic");
+                break;
             }
         }
-
-        if(isChaotic){
-            System.out.println("Too chaotic");
-        }else{
+        if(!isChaotic){
             System.out.println(bribes);
         }
+    }
+
+    private static void swap(int[] arr,int a,int b){
+        int temp = arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
     }
 
     public static void main(String[] args) {
