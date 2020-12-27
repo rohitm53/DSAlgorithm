23a0dac72190a519a0634be9f1d1052a117c7ebb
rohitm <rohit.manohar@tomra.com>
2020-12-27 21:18:02 +0530

    f

---
+++ src/problems/arrays/MinimunSwap.java
@@ -0,0 +1,20 @@
+package problems.arrays;
+
+public class MinimunSwap {
+
+    static int minimumSwaps(int[] arr) {
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr={2,3,4,1,5};
+
+        int res = minimumSwaps(arr);
+        System.out.println(res);
+
+    }
+
+}
---
+++ src/problems/arrays/sorting/QuickSortAlgorithm.java
@@ -0,0 +1,54 @@
+package problems.arrays.sorting;
+
+import java.util.Arrays;
+
+public class QuickSortAlgorithm {
+
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,1,10,6,9,8};
+        int low = 0;
+        int high = arr.length-1;
+        quickSort(arr,low,high);
+
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+
+    private static void quickSort(int[] arr , int low,int high) {
+
+        if(low<high){
+            int pivotIndex=partition(arr,low,high);
+            quickSort(arr,low,pivotIndex);
+            quickSort(arr,pivotIndex+1,high);
+        }
+
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[low];
+        int i=low,j=high;
+
+        while(i<j){
+
+          while(i<arr.length && pivot>=arr[i]){
+              i++;
+          }
+
+          while(pivot<arr[j]){
+              j--;
+          }
+          if(i<j){
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,low,j);
+        return j;
+    }
+
+    private static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+}
